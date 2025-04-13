b192f2a8e6ed888d651d0bee2ecb6995f0150517
KadeoPL <stopkakrzysztof1@gmail.com>
2025-04-13 22:43:32 +0200

    create play button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,25 +1,7 @@
 export default function Button({ text }: { text: string }) {
   return (
-    <button
-      className={`
-    relative px-60 py-3
-    w-auto min-w-[200px]
-    inline-block text-center
-    border-solid border-8
-    border-image-source-[url('/public/home_bg.png')]
-    border-image-slice-9
-    border-image-repeat-stretch
-    transition-transform duration-100
-    
-  `}
-      style={{
-        backgroundImage: "url('/public/home_bg.png')",
-        backgroundSize: "cover",
-      }}
-    >
-      <span className="relative text-white font-bold tracking-wider text-xl">
-        {text}
-      </span>
+    <button className="start-button font-pirata uppercase text-2xl hover:scale-105 transiton-all duration-700 ease-in-out">
+      <span className="drop-shadow-2xl text-gray-300">{text}</span>
     </button>
   );
 }
