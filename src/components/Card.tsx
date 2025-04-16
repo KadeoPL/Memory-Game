d89f5f721d3b111e777170b210c18d93ff264093
KadeoPL <stopkakrzysztof1@gmail.com>
2025-04-16 22:03:05 +0200

    add flip back animate

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,21 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Card() {
   const [isCardFlip, setIsCardFlip] = useState(false);
+  const [animationClass, setAnimationClass] = useState("");
+
+  useEffect(() => {
+    isCardFlip
+      ? setAnimationClass("animate-flip")
+      : setAnimationClass("animate-flip-back");
+  }, [isCardFlip]);
 
   return (
     <>
       {isCardFlip ? (
         <div
           onClick={() => setIsCardFlip(false)}
-          className="bg-green-500 h-30 w-20 rounded-xl drop-shadow-2xl hover:scale-105 transition-all duration-150 ease-in animate-flip"
+          className={`bg-green-500 h-30 w-20 rounded-xl drop-shadow-2xl hover:scale-105 transition-all duration-150 ease-in ${animationClass}`}
         >
           Card
         </div>
       ) : (
         <div
           onClick={() => setIsCardFlip(true)}
-          className="bg-red-500 h-30 w-20 rounded-xl drop-shadow-2xl hover:scale-105 transition-all duration-150 ease-in animate-flip"
+          className={`bg-red-500 h-30 w-20 rounded-xl drop-shadow-2xl hover:scale-105 transition-all duration-150 ease-in ${animationClass}`}
         >
           Card
         </div>
