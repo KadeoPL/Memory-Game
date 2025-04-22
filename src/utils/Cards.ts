ef3ff86a971f4f95fe37502a0565f2d36110d5b5
KadeoPL <stopkakrzysztof1@gmail.com>
2025-04-22 14:54:58 +0200

    fix card import

--- src/utils/Cards.ts
+++ src/utils/Cards.ts
@@ -1,64 +1,74 @@
+import card1 from "../assets/cards/front_card_320x480_1.png";
+import card2 from "../assets/cards/front_card_320x480_2.png";
+import card3 from "../assets/cards/front_card_320x480_3.png";
+import card4 from "../assets/cards/front_card_320x480_4.png";
+import card5 from "../assets/cards/front_card_320x480_5.png";
+import card6 from "../assets/cards/front_card_320x480_6.png";
+import card7 from "../assets/cards/front_card_320x480_7.png";
+import card8 from "../assets/cards/front_card_320x480_8.png";
+import card9 from "../assets/cards/front_card_320x480_9.png";
+
 export const cards = [
   {
     id: 1,
     name: "Shadow Wraith",
-    image: "src/assets/cards/front_card_320x480_1.png",
+    image: card1,
     isFlipped: false,
     isMatched: false,
   },
   {
     id: 2,
     name: "Blood Moon",
-    image: "src/assets/cards/front_card_320x480_2.png",
+    image: card2,
     isFlipped: false,
     isMatched: false,
   },
   {
     id: 3,
     name: "Cursed Relic",
-    image: "src/assets/cards/front_card_320x480_3.png",
+    image: card3,
     isFlipped: false,
     isMatched: false,
   },
   {
     id: 4,
     name: "Spectral Chains",
-    image: "src/assets/cards/front_card_320x480_4.png",
+    image: card4,
     isFlipped: false,
     isMatched: false,
   },
   {
     id: 5,
     name: "Soul Devourer",
-    image: "src/assets/cards/front_card_320x480_5.png",
+    image: card5,
     isFlipped: false,
     isMatched: false,
   },
   {
     id: 6,
     name: "Bone Whisperer",
-    image: "src/assets/cards/front_card_320x480_6.png",
+    image: card6,
     isFlipped: false,
     isMatched: false,
   },
   {
     id: 7,
     name: "Cryptic Tomb",
-    image: "src/assets/cards/front_card_320x480_7.png",
+    image: card7,
     isFlipped: false,
     isMatched: false,
   },
   {
     id: 8,
     name: "Void Phantom",
-    image: "src/assets/cards/front_card_320x480_8.png",
+    image: card8,
     isFlipped: false,
     isMatched: false,
   },
   {
     id: 9,
     name: "Void",
-    image: "src/assets/cards/front_card_320x480_9.png",
+    image: card9,
     isFlipped: false,
     isMatched: false,
   },
