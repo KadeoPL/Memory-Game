3335ef15acbbb25e40ae17bf9e3e971c3906102e
KadeoPL <stopkakrzysztof1@gmail.com>
2025-04-23 14:35:35 +0200

    Merge

--- src/types/ButtonProps.ts
+++ src/types/ButtonProps.ts
@@ -1,9 +1,4 @@
 export interface ButtonProps {
   text: string;
-<<<<<<< HEAD:src/types/ButtonProps.ts
   animationPulse: boolean;
-=======
-  moves?: number;
-  onClick?: () => void;
->>>>>>> fd8523c98e81f6afa289494f49fc25527607aad9:src/types/ButtonProps.tsx
 }
